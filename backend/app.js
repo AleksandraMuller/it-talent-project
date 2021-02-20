4c337eb396d4d3f2d7d449122df2ded650c676d7
Aleksandra <aleksandra.e.mueller@gmail.com>
2021-02-20 22:59:07 +0100

    set up

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 //allow cross-origin requests
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 
 
@@ -42,7 +42,7 @@ app.use(
 );
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', './build/index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/build', '../frontend/build/index.html'));
 });
 
 app.listen(port, () => {
