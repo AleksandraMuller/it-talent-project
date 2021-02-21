0fb71011122786935b8a959c83065846d4259e0c
Aleksandra <aleksandra.e.mueller@gmail.com>
2021-02-21 13:07:46 +0100

    check

--- backend/app.js
+++ backend/app.js
@@ -39,7 +39,9 @@ app.use(
 );
 
 app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+	res.sendFile(
+		path.join(__dirname, '../frontend/build', '../frontend/build/index.html')
+	);
 	res.sendFile(path.resolve(__dirname + '../frontend/build/index.html'));
 });
 
