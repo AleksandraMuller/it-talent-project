8fb5c3e8e5aaf44816029850221fb7b01a5410a6
Aleksandra <aleksandra.e.mueller@gmail.com>
2021-02-21 13:46:09 +0100

    build fix

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 //allow cross-origin requests
 app.use(cors());
 
-app.use(express.static(path.join('../frontend/build')));
+// app.use(express.static(path.join('../frontend/build')));
 
 const mongoUrl = process.env['MONGO_URL'];
 
@@ -38,8 +38,9 @@ app.use(
 	})
 );
 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '/client/build/index.html'));
+app.use(express.static('/frontend/build'));
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
 });
 
 app.listen(port, () => {
