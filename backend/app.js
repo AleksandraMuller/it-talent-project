d41575c017de0c1870f90f1f1ee9b230cb8e4a32
Aleksandra <aleksandra.e.mueller@gmail.com>
2021-02-21 12:54:46 +0100

    route fix?

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,12 @@ import { graphqlHTTP } from 'express-graphql';
 import schema from './schema/schema.js';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import path from 'path';
+// import path from 'path';
 
 //ADD TO USE ENV VARIABLE
 require('dotenv').config();
+
+let protected = ['transformed.js', 'main.css', 'favicon.ico'];
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,7 +16,7 @@ const port = process.env.PORT || 5000;
 //allow cross-origin requests
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+// app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 const mongoUrl = process.env['MONGO_URL'];
 
@@ -38,9 +40,21 @@ app.use(
 	})
 );
 
-app.get('/*', (req, res) => {
-	// res.sendFile(path.join(__dirname, '../frontend/build', '../frontend/build/index.html'));
-	res.sendFile(path.resolve(__dirname + '../frontend/build/index.html'));
+// app.get('/*', (req, res) => {
+// 	res.sendFile(path.join(__dirname, '../frontend/build', '../frontend/build/index.html'));
+// 	res.sendFile(path.resolve(__dirname + '../frontend/build/index.html'));
+// });
+
+app.get('*', (req, res) => {
+	let path = req.params['0'].substring(1);
+
+	if (protected.includes(path)) {
+		// Return the actual file
+		res.sendFile(`${__dirname}/frontend/build/${path}`);
+	} else {
+		// Otherwise, redirect to /build/index.html
+		res.sendFile(`${__dirname}/frontend/build/index.html`);
+	}
 });
 
 app.listen(port, () => {
