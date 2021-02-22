d3abb0d7f80cb96931a788c10334cccdd7063112
Aleksandra <aleksandra.e.mueller@gmail.com>
2021-02-22 12:06:01 +0100

    last changes

--- frontend/src/pages/StartPage.jsx
+++ frontend/src/pages/StartPage.jsx
@@ -315,20 +315,16 @@ const StartPage = () => {
 				</StyledContainerColored>
 			</StyledFlexbox>
 			<StyledWrapper>
-				<Accordion title='Why is the sky blue?'>
-					Sunlight reaches Earth's atmosphere and is scattered in all directions
-					by all the gases and particles in the air. Blue light is scattered
-					more than the other colors because it travels as shorter, smaller
-					waves. This is why we see a blue sky most of the time.
+				<Accordion title='Why do we need a whiteboard?'>
+					To share with others the greatest ideas and to make brainstorming
+					easier.
 				</Accordion>
-				<Accordion title="What's It Like Inside Jupiter?">
-					It's really hot inside Jupiter! No one knows exactly how hot, but
-					scientists think it could be about 43,000°F (24,000°C) near Jupiter's
-					center, or core.
+				<Accordion title='When are the new colors in the whiteboard coming?'>
+					Soon! The next release is planned for the end of February.
 				</Accordion>
-				<Accordion title='What Is a Black Hole?'>
-					A black hole is an area of such immense gravity that nothing -- not
-					even light -- can escape from it.
+				<Accordion title='Where can I save my stickies?'>
+					At the moment you can not save your stickies, but we are working on
+					the solution to this issue.
 				</Accordion>
 			</StyledWrapper>
 
